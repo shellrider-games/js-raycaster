fc7df0d7487f26574c90b64e23d97bbe51c7bc03
Georg Becker <georg.becker@shellrider.com>
2024-01-17 18:02:05 +0100

    Adds movement and rotation

--- src/main.js
+++ src/main.js
@@ -1,3 +1,39 @@
+const gameData = {
+    inputMap : {
+        left: false,
+        right: false,
+        up: false,
+        down: false,
+        rot_left: false,
+        rot_right: false,
+    }
+}
+
+const clampToLevel = (pos, level) => {
+    return {
+        x: Math.min(Math.max(pos.x, 1.5),level.width-1.5),
+        y: Math.min(Math.max(pos.y, 1.5),level.heigth-1.5)
+    }
+}
+
+const inputVector = (inputMap) => {
+    let x = inputMap.right - inputMap.left;
+    let y = inputMap.down - inputMap.up;
+    if(x != 0 || y != 0) {
+        let len = Math.sqrt(x*x + y*y);
+        x = x/len;
+        y = y/len;
+    }
+    return {
+        x: x,
+        y: y
+    };
+}
+
+const rotationVector = (inputMap) => {
+    return inputMap.rot_right - inputMap.rot_left;
+}
+
 const getLevel = (w,h, strategy) => {
     const tiles = new Array(w*h).fill(0).map((_,idx) => {
         let x = idx % w;
@@ -16,9 +52,10 @@ const getCamera = (x, y, canvas) => {
     return {
         x: x,
         y: y,
+        dir: 0,
         w: canvas.width,
         h: canvas.height,
-        fov: 1.23, // about 70 deg
+        fov: 1.23,
         farClip: 20
     };
 }
@@ -39,17 +76,54 @@ const distanceToWall = (x, y, angle, level, maxDistance, step) => {
     return undefined;
 }
 
-const drawLevel = (level, cam, ctx) => {
+const clearCanvas = (cam, ctx) => {
+    ctx.clearRect(0,0, cam.w, cam.h);
+}
+
+const drawFloorAndCeiling = (cam, ctx) => {
     ctx.fillStyle = '#555555';
     ctx.fillRect(0,0, cam.w, cam.h/2);
     ctx.fillStyle = '#888888';
     ctx.fillRect(0,cam.h/2, cam.w, cam.h/2);
+}
+
+const update = (timestamp) => {
+    let delta = timestamp - gameData.lastTimestamp;
+    let movSpeed = 0.004;
+    let rotSpeed = 0.008;
+    gameData.lastTimestamp = timestamp;
+    let rot = rotationVector(gameData.inputMap);
+    
+    gameData.cam.dir = (gameData.cam.dir + (rot * rotSpeed * delta)) % (2*Math.PI);
+
+    let vec = {
+        x: (Math.cos(gameData.cam.dir) * inputVector(gameData.inputMap).x - Math.sin(gameData.cam.dir) * inputVector(gameData.inputMap).y),
+        y: (Math.sin(gameData.cam.dir) * inputVector(gameData.inputMap).x + Math.cos(gameData.cam.dir) * inputVector(gameData.inputMap).y)
+    };
+
+    console.log(vec);
+
+    const nextPosition = clampToLevel({
+        x: gameData.cam.x + vec.x * movSpeed * delta,
+        y: gameData.cam.y + vec.y * movSpeed * delta
+    }, gameData.level);
+
+    gameData.cam.x = nextPosition.x;
+    gameData.cam.y = nextPosition.y;
+
+    drawLevel(gameData.level, gameData.cam, gameData.ctx);
+
+    requestAnimationFrame(update);
+}
+
+const drawLevel = (level, cam, ctx) => {
+    drawFloorAndCeiling(cam, ctx);
     ctx.fillStyle = 'blue';
     const fovPerColumn = cam.fov/cam.w;
+    const dir = cam.dir - (Math.PI/2);
     for(let x = 0; x < cam.w; x++){
-        let dir = Math.PI/10;
         const rayAngle = dir+x*fovPerColumn-cam.fov/2;
-        const distance = distanceToWall(cam.x, cam.y, rayAngle, level, cam.farClip, 0.05);
+        const distance = distanceToWall(cam.x, cam.y, rayAngle, level, cam.farClip, 0.1);
         if(!distance) { continue; }
         let correctedDistance = distance * Math.cos(rayAngle - dir);
         const lineHeight = cam.h/correctedDistance;
@@ -64,14 +138,66 @@ const edgeTileStrategy = (x,y,w,h) => {
 }
 
 const startGame = () => {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    let level = getLevel(16,12,edgeTileStrategy);
-    const cam = getCamera(12,10, canvas);
-    drawLevel(level, cam, ctx);
+    gameData.canvas = document.getElementById('canvas');
+    gameData.ctx = canvas.getContext('2d');
+    gameData.level = getLevel(16,12,edgeTileStrategy);
+    gameData.cam = getCamera(12,10, canvas);
+    gameData.lastTimestamp = 0;
+    requestAnimationFrame(update);
 }
 
 window.onload = () => {
     console.log("Window loaded, starting game.");
     startGame();
 }
+
+addEventListener("keydown", (ev) => {
+    switch(ev.key) {
+        case "ArrowDown":
+            gameData.inputMap.down = true;
+            break;
+        case "ArrowUp":
+            gameData.inputMap.up = true;
+            break;
+        case "ArrowLeft":
+            gameData.inputMap.left = true;
+            break;
+        case "ArrowRight":
+            gameData.inputMap.right = true;
+            break;
+        case "a":
+            gameData.inputMap.rot_left = true;
+            break;
+        case "d":
+            gameData.inputMap.rot_right = true;
+            break;
+        default:
+            return
+    }
+});
+
+addEventListener("keyup", (ev) => {
+    switch(ev.key) {
+        case "ArrowDown":
+            gameData.inputMap.down = false;
+            break;
+        case "ArrowUp":
+            gameData.inputMap.up = false;
+            break;
+        case "ArrowLeft":
+            gameData.inputMap.left = false;
+            break;
+        case "ArrowRight":
+            gameData.inputMap.right = false;
+            break;
+        case "a":
+            gameData.inputMap.rot_left = false;
+            break;
+        case "d":
+            gameData.inputMap.rot_right = false;
+            break;
+        
+        default:
+            return
+    }
+});
