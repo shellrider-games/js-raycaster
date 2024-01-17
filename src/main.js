9c3fa60cfca2de1036dacddb5461dea7585129b2
Georg Becker <georg.becker@shellrider.com>
2024-01-17 09:51:36 +0100

    Draws blue rectangle to screen

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
+const startGame = () => {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'blue';
+    ctx.fillRect((canvas.width - 50) * 0.5, (canvas.height-50) * 0.5, 50, 50)
+}
+
 window.onload = () => {
-    console.log("Hello, World!");
+    console.log("Window loaded, starting game.");
+    startGame();
 }
