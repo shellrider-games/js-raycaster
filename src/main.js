5051da3829ead41ab8a73e0aafd87b0378e53e38
Georg Becker <georg.becker@shellrider.com>
2024-01-17 10:23:56 +0100

    Setup level and a drawing logic

--- src/main.js
+++ src/main.js
@@ -1,9 +1,38 @@
-const startGame = () => {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
+const getLevel = (w,h, strategy) => {
+    const tiles = new Array(w*h).fill(0).map((_,idx) => {
+        let x = idx % w;
+        let y = Math.floor(idx/w);
+        return strategy(x,y,w,h);
+    });
 
-    ctx.fillStyle = 'blue';
-    ctx.fillRect((canvas.width - 50) * 0.5, (canvas.height-50) * 0.5, 50, 50)
+    return {
+        width: w,
+        heigth: h,
+        tiles: tiles
+    };
+}
+
+const drawLevel = (level, ctx) => {
+    let tileSize = 16;
+    ctx.fillStyle = "blue";
+    level.tiles.forEach((val, idx) => {
+        if(val > 0) {
+            let x = idx % level.width;
+            let y = Math.floor(idx/level.width);
+            ctx.fillRect(x*tileSize,y*tileSize,tileSize,tileSize);
+        }
+    })
+}
+
+const edgeTileStrategy = (x,y,w,h) => {
+    return (x === 0 || x === w - 1 || y === 0 || y === h - 1) ? 1 : 0;
+}
+
+const startGame = () => {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    let level = getLevel(16,12,edgeTileStrategy);
+    drawLevel(level,ctx);
 }
 
 window.onload = () => {
